287c81586806dc12500557f79b1ff4ecb4fbcb3e
DillonMUMDGithub <dillon.miller2002@gmail.com>
2024-03-13 11:03:03 -0400

    Added ObstacleMatTest.py, addedd ependencies for Main
    Imported ObstacleMatTest, a file that designates the C2C Matrix as well as the objects. Additionally I created a function to calculate the euclidian distance from a goal point to assign C2G. Will eventually move this code into main, and as of now C2G isnt modular for evey goal point

--- Proj3_Dillon_Caleb_Hamsaavarthan.py
+++ Proj3_Dillon_Caleb_Hamsaavarthan.py
@@ -1,2 +1,12 @@
 """Project 3, A* Algorithm implementation
     """
+    
+    
+from ObstacleMatTest import ObstMatC2C,ObstMatC2G
+import matplotlib.pyplot as plt
+import time as time
+from queue import PriorityQueue
+import numpy as np
+import random
+import os
+import cv2 as cv
